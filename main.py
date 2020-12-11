83d53d190ff0c069c877c987790b9555195aa00b
Astropolitans <paul.bokelman1@gmail.com>
2020-12-10 20:26:37 -0800

    added venv for other text editors

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, redirect, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sit.db'
 
 @app.route("/home")
 @app.route("/")
